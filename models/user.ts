6e9accc9005c60caf9fc87211a9ed91d628af4ed
dannesart <dannesart@live.com>
2023-06-26 23:31:19 +0200

    Some copy

--- models/user.ts
+++ models/user.ts
@@ -4,6 +4,7 @@ const UserSchema = z.object({
   name: z.string().min(1).max(100),
   email: z.string().email(),
   id: z.string(),
+  sub: z.string().optional(),
   nickname: z.string().min(1),
   image: z.string().optional(),
 });
--- pages/user/index.vue
+++ pages/user/index.vue
@@ -8,18 +8,53 @@
         </UIHeadline>
         <UIList>
             <UIListItem>
-                <strong>Name</strong> {{ data?.user?.name }}
+                <strong>Name</strong> {{ user?.name }}
             </UIListItem>
             <UIListItem>
-                <strong>Email</strong> {{ data?.user?.email }}
+                <strong>Email</strong> {{ user?.email }} <UIIcons v-if="user?.email_verified" name="check-badge"
+                    :class="'text-green-500'">
+                </UIIcons>
             </UIListItem>
         </UIList>
+        <UIHeadline size="h2">
+            Info
+        </UIHeadline>
+        <UIList>
+            <UIListItem>
+                <strong>Last login</strong> {{ user?.last_login }}
+            </UIListItem>
+            <UIListItem>
+                <strong>ID</strong> {{ user?.user_id }}
+            </UIListItem>
+        </UIList>
+        <UIHeadline size="h2">
+            Other
+        </UIHeadline>
+        <section class="flex flex-col gap-6 p-6 bg-white rounded-xl shadow-lg">
+            <label>
+                I quit
+            </label>
+            <p class="text-sm text-gray-400">
+                Please remove me as a user from meshflow for ever. <br />
+                This means, that you will be removed from every project you are part of. <br />
+                And removed as a user in meshflow. You'll however be able to join again in the future if you should change
+                your
+                mind.
+            </p>
+            <div>
+                <UIButton type="nevermind">Goodbye</UIButton>
+            </div>
+        </section>
 
     </NuxtLayout>
 </template>
 
 <script setup lang="ts">
+import { useAuthStore } from '~~/stores/auth';
+import { useUsersStore } from '~~/stores/users';
 
 const { data, signOut } = useSession()
-
+const authStore = useAuthStore();
+const usersStore = useUsersStore();
+const user = usersStore.userById(authStore.currentUser.sub || '') || {};
 </script>
