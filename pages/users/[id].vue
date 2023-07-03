de7f7e26da499f1df581026c39663ec025a70478
dannesart <dannesart@live.com>
2023-07-03 15:38:02 +0200

    Users page

--- pages/users/[id].vue
+++ pages/users/[id].vue
@@ -1,12 +1,19 @@
 <template>
     <NuxtLayout>
         <UIHeadline size="h1">
-            User with id {{ id }}
+            {{ user.name }}
         </UIHeadline>
-        ...
+        <p>
+            Last logged in <strong>{{ useTimeAgo(user.last_login) }}</strong>
+        </p>
     </NuxtLayout>
 </template>
 
 <script setup>
+
+import { useUsersStore } from "~~/stores/users";
+
 const { id } = useRoute().params;
+const { userById } = useUsersStore();
+const user = userById(id);
 </script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -3,6 +3,19 @@
         <UIHeadline size="h1">
             Users
         </UIHeadline>
-        ...
+        <ul class="w-full flex gap-5">
+            <li v-for="(user, userId) in allUsers" class="border-b last:border-b-0 bg-white shadow-lg rounded-lg p-5 w-52 ">
+
+                <ModulesExtendedLink :label="user.name" :style="'small'" :sub-label="useTimeAgo(user.last_login)"
+                    :route="('/users/' + userId)" :image="user.picture">
+                </ModulesExtendedLink>
+            </li>
+        </ul>
     </NuxtLayout>
 </template>
+
+<script setup>
+import { useUsersStore } from "~~/stores/users";
+
+const { allUsers } = useUsersStore();
+</script>
