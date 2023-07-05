74d8f9be0e7847bcbe71bebc1552aa81fdd1685f
dannesart <dannesart@live.com>
2023-07-05 10:10:11 +0200

    Testing config with back up

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -6,9 +6,18 @@ const config = useRuntimeConfig();
 export default NuxtAuthHandler({
   providers: [
     Providers.default({
-      clientId: config.clientId || process.env.AUTH0_CLIENT_ID,
-      clientSecret: config.clientSecret || process.env.AUTH0_CLIENT_SECRET,
-      issuer: config.issuer || process.env.AUTH0_ISSUER,
+      clientId:
+        config.clientId ||
+        process.env.AUTH0_CLIENT_ID ||
+        "wYKLHxV9LPWMzvwZmmro7wemafsO4dsU",
+      clientSecret:
+        config.clientSecret ||
+        process.env.AUTH0_CLIENT_SECRET ||
+        "GrNNZ7EsS-N9nS_G7wkaFtEtkqTvDTHsjo-13GBuJFa0Cnt40MmcOf5hVk--SkV9",
+      issuer:
+        config.issuer ||
+        process.env.AUTH0_ISSUER ||
+        "https://meshflow.eu.auth0.com",
       domain: config.domain || process.env.AUTH0_DOMAIN,
     }),
   ],
