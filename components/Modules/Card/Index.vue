9a5b46fc0275d2369371c9fcc2950399703115e7
dannesart <dannesart@live.com>
2023-10-12 19:10:42 +0200

    Updating card design
    Removing unncessary margins

--- components/Modules/Card/Index.vue
+++ components/Modules/Card/Index.vue
@@ -2,18 +2,18 @@
     <div class="rounded-xl shadow-lg bg-white relative hover:shadow-xl hover:cursor-pointer"
         :class="{ 'p-6 py-4': size === 'small', 'p-10 py-8': size !== 'small' }">
 
-        <UIHeadline size="h3" data-title :class="'pr-12'">
+        <UIHeadline size="h3" data-title :class="{ 'pr-12': true }">
             {{ title }}
         </UIHeadline>
 
-        <p class="first-letter:capitalize truncate"
-            :class="{ 'text-slate-400': body !== 'public' && body !== 'private', 'text-emerald-400': body === 'public', 'text-red-400': body === 'private' }"
+        <p class="first-letter:capitalize truncate" v-if="body"
+            :class="{ 'mb-0': !tags.length, 'pr-12': true, 'text-slate-400': body !== 'public' && body !== 'private', 'text-emerald-400': body === 'public', 'text-red-400': body === 'private' }"
             data-body>
             {{ body }}
         </p>
         <ModulesFavorite :favorite="favoriteState" @favorite="toggleFavorite" :size="size"></ModulesFavorite>
 
-        <ModulesTagsList :can-add="false" v-if="tags" :class="'mt-4'">
+        <ModulesTagsList :can-add="false" v-if="tags && tags.length" :class="'mt-4'">
             <UITag v-for="tag in tags" class="px-2 py-0.5 text-sm">
                 {{ tag }}
             </UITag>
