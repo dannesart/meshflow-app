3fa35d39244cedb94cf7e321d12879c4f1cbf652
dannesart <dannesart@live.com>
2023-10-16 23:48:25 +0200

    Update [id].vue

--- pages/blocks/[type]/[id].vue
+++ pages/blocks/[type]/[id].vue
@@ -5,15 +5,20 @@
         </UIHeadline>
         <div class="flex gap-6">
             Block with a type of {{ type }} and id {{ id }}
+            <div v-if="block">
+                {{block}}
+            </div>
         </div>
     </NuxtLayout>
 </template>
 
 <script setup>
-
+import { useBlocksStore } = from '~~/stores/blocks';
+import { storeToRefs } from 'pinia';
 const { id, type } = useRoute().params;
 
     // Fetch data based on id.
+    const block = computed(() => getBlockById(type as string, id as string));
 
     // Show data.
 
