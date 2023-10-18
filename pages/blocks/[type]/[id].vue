bc9b50f7edae86193c19cc01648a8b1223553274
Daniel <daniel.martensson@sgdsgruppen.se>
2023-10-18 17:11:24 +0200

    get block by id and not name inside blocks types

--- pages/blocks/[type]/[id].vue
+++ pages/blocks/[type]/[id].vue
@@ -1,29 +1,31 @@
 <template>
-    <NuxtLayout>
-        <UIHeadline size="h1">
-            {{ id }}
-        </UIHeadline>
-        <div class="flex gap-6">
-            Block with a type of {{ type }} and id {{ id }}
-            <div v-if="block">
-                {{block}}
-            </div>
-        </div>
-    </NuxtLayout>
+  <NuxtLayout>
+    <UIHeadline size="h1">
+      {{ id }}
+    </UIHeadline>
+    <div class="flex gap-6">
+      Block with a type of {{ type }} and id {{ id }}
+      <div v-if="block">
+        {{ block }}
+      </div>
+    </div>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-import { useBlocksStore } from '~~/stores/blocks';
-import { storeToRefs } from 'pinia';
+import { useBlocksStore } from "~~/stores/blocks";
+import { storeToRefs } from "pinia";
 const { id, type } = useRoute().params;
-const { getBlockById, getBlocksByType, fetchBlocks, getBlockModelById } = useBlocksStore();
+const { getBlockById, getBlocksByType, fetchBlocks, getBlockModelById } =
+  useBlocksStore();
 const blockType = computed(() => getBlockModelById(type as string));
-    
-if(!getBlocksByType(blockType.value?.name)) fetchBlocks(blockType.value?.name);
+
+if (!getBlocksByType(blockType.value?.id)) fetchBlocks(blockType.value?.id);
 
 // Fetch data based on id.
-const block = computed(() => getBlockById(blockType.value?.name as string, id as string));
+const block = computed(() =>
+  getBlockById(blockType.value?.id as string, id as string)
+);
 
 // Show data.
-
 </script>
