571d8afbea6e9f3fe47d88e064733daa6b7e2493
Daniel <daniel.martensson@sgdsgruppen.se>
2023-10-30 00:09:46 +0100

    Fixing broken grid-cols css-class

--- components/UI/Grid/Index.vue
+++ components/UI/Grid/Index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="grid grid-cols-[repeat(auto-fit,minmax(min(100%, 180px),1fr))]">
+  <div
+    class="grid grid-cols-[repeat(auto-fit,minmax(min(100%,180px),1fr))] gap-6"
+  >
     <slot />
   </div>
 </template>
