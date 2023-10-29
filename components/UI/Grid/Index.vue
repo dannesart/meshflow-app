d5be7401b209a2f0763b14c78f5c548402162091
Daniel <daniel.martensson@sgdsgruppen.se>
2023-10-29 23:59:19 +0100

    Adding grid component

---
+++ components/UI/Grid/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="grid grid-cols-[repeat(auto-fit,minmax(min(100%, 180px),1fr))]">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<script lang="ts">
+export default defineComponent({
+  name: "Grid",
+});
+</script>
