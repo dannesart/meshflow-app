4a7bcf50d15324fe66e8611711ecc8a4a3c7bf83
Daniel <daniel.martensson@sgdsgruppen.se>
2023-10-30 00:00:48 +0100

    First use of grid component inside users route

--- pages/roles/index.vue
+++ pages/roles/index.vue
@@ -1,58 +1,60 @@
 <template>
-    <NuxtLayout>
+  <NuxtLayout>
+    <UIHeadline size="h1"> Roles </UIHeadline>
+    <div class="flex gap-6">
+      <ModulesFilter @filterChange="filterChange" @sortChange="sortChange">
+      </ModulesFilter>
+      <div class="w-[1px] h-5/6 my-auto bg-slate-300"></div>
+      <ModulesAdd> </ModulesAdd>
+    </div>
 
-        <UIHeadline size="h1">
-            Roles
-        </UIHeadline>
-        <div class="flex gap-6">
-            <ModulesFilter @filterChange="filterChange" @sortChange="sortChange">
-            </ModulesFilter>
-            <div class="w-[1px] h-5/6 my-auto bg-slate-300 "></div>
-            <ModulesAdd>
-            </ModulesAdd>
-        </div>
-
-        <div class="flex md:gap-6 flex-col md:flex-row">
-
-            <NuxtLink :to="('/roles/' + item.id)" class="flex-1 md:max-w-md" v-for="(item, index) in data">
-                <Card :title="item.title" :body="item.body" :favorite="item.favorite"
-                    @favorite="event => updateFavorite(event, index)"></Card>
-            </NuxtLink>
-
-        </div>
-
-    </NuxtLayout>
+    <UIGrid>
+      <NuxtLink
+        :to="'/roles/' + item.id"
+        class="flex-1 md:max-w-md"
+        v-for="(item, index) in data"
+        :key="item.id"
+      >
+        <Card
+          :title="item.title"
+          :body="item.body"
+          :favorite="item.favorite"
+          @favorite="(event) => updateFavorite(event, index)"
+        ></Card>
+      </NuxtLink>
+    </UIGrid>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 const filters = ref({});
 const sorts = ref({});
 const data = ref([
-    {
-        title: "Admin",
-        body: "Can do anything",
-        id: "admin-role",
-        favorite: false
-    },
-    {
-        title: "Publisher",
-        body: "Can publish data, but not handle users",
-        id: "publisher-role",
-        favorite: false
-    }
-])
+  {
+    title: "Admin",
+    body: "Can do anything",
+    id: "admin-role",
+    favorite: false,
+  },
+  {
+    title: "Publisher",
+    body: "Can publish data, but not handle users",
+    id: "publisher-role",
+    favorite: false,
+  },
+]);
 
 const updateFavorite = (favorite: any, itemIdx: number) => {
-    const newData = [...data.value]
-    newData[itemIdx] = { ...newData[itemIdx], favorite }
-    data.value = newData;
-}
+  const newData = [...data.value];
+  newData[itemIdx] = { ...newData[itemIdx], favorite };
+  data.value = newData;
+};
 
 const filterChange = async (_filters: { [key: string]: any }) => {
-    filters.value = { ...filters.value, ..._filters }
-}
+  filters.value = { ...filters.value, ..._filters };
+};
 
 const sortChange = async (_sorts: { [key: string]: any }) => {
-    sorts.value = { ...sorts.value, ..._sorts }
-}
+  sorts.value = { ...sorts.value, ..._sorts };
+};
 </script>
