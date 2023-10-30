ce222dde955bd0885645db61e57d362be89dd7c9
Daniel <daniel.martensson@sgdsgruppen.se>
2023-10-30 01:12:01 +0100

    update in signin logo

--- pages/signin.vue
+++ pages/signin.vue
@@ -3,7 +3,7 @@
     <div
       class="relative flex flex-col gap-6 p-10 py-8 mb-0 bg-white shadow-lg rounded-xl"
     >
-      <div>
+      <div class="flex items-center gap-6 text-3xl font-bold text-emerald-400">
         <UILogo :size="40" />
         <span
           class="hidden text-xl font-extrabold md:block lg:text-2xl font-Rounded"
