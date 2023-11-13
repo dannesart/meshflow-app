277e7149a61295c7690d26b831e438e838968d68
Daniel <daniel.martensson@sgdsgruppen.se>
2023-11-13 23:33:46 +0100

    Updating logo design

--- components/Modules/Aside/index.vue
+++ components/Modules/Aside/index.vue
@@ -1,61 +1,73 @@
 <template>
-    <aside
-        class="h-full py-4 px-6 lg:px-10 flex flex-col gap-8 transition-transform z-40 bg-white md:inset-auto md:relative"
-        v-bind:class="{ 'absolute inset-0 ': menuState }">
-        <div class="w-3/4 text-emerald-400 text-3xl font-bold md:px-4 rounded-full flex items-center flex-none h-14">
-            <NuxtLink to="/" class="flex gap-6 items-center">
-
-                <UILogo :size="40" />
-                <span class="hidden md:block text-xl lg:text-2xl font-Rounded font-extrabold ">meshflow</span>
+  <aside
+    class="z-40 flex flex-col h-full gap-8 px-6 py-4 transition-transform bg-white lg:px-10 md:inset-auto md:relative"
+    v-bind:class="{ 'absolute inset-0 ': menuState }"
+  >
+    <div
+      class="flex items-center flex-none w-3/4 text-3xl font-bold rounded-full text-emerald-400 md:px-4 h-14"
+    >
+      <NuxtLink to="/" class="flex items-center gap-6">
+        <UILogo :size="30" />
+        <span
+          class="hidden text-xl font-extrabold md:block lg:text-2xl font-Rounded"
+          >meshflow</span
+        >
+      </NuxtLink>
+      <button class="px-3 py-3 ml-3 md:hidden" @click="toggleMenu()">
+        <UIIcons :name="menuState ? 'close' : 'menu'" :size="8"></UIIcons>
+      </button>
+    </div>
+    <nav
+      class="flex flex-col gap-3"
+      v-bind:class="{ 'hidden md:flex': !menuState }"
+    >
+      <div v-for="group in Menu" :key="group.label">
+        <label
+          v-if="group.label"
+          class="block my-3 text-sm text-gray-400 md:px-5"
+        >
+          {{ group.label }}
+        </label>
+        <ul class="flex flex-col gap-1">
+          <li v-for="item in group.items" :key="item.route">
+            <NuxtLink
+              :to="item.route"
+              v-if="(item.webOnly && settingsStore.isWeb) || !item.webOnly"
+              class="flex items-center gap-3 py-3 text-2xl text-gray-800 rounded-lg md:text-lg md:hover:bg-emerald-100 hover:text-emerald-700 md:px-5"
+              activeClass="font-bold text-emerald-700 md:bg-emerald-100"
+              router-link-active="font-bold text-emerald-700 bg-emerald-100"
+            >
+              <UIIcons
+                :name="item.icon"
+                v-if="item.icon && showIcons"
+              ></UIIcons>
+              {{ item.label }}
             </NuxtLink>
-            <button class="md:hidden px-3 py-3 ml-3" @click="toggleMenu()">
-                <UIIcons :name="menuState ? 'close' : 'menu'" :size="8"></UIIcons>
-            </button>
-
-
-        </div>
-        <nav class="flex flex-col gap-3" v-bind:class="{ 'hidden md:flex': !menuState }">
-            <div v-for="group in Menu">
-                <label v-if="group.label" class="text-sm text-gray-400 md:px-5 my-3 block">
-                    {{ group.label }}
-                </label>
-                <ul class="flex flex-col gap-1">
-                    <li v-for="item in group.items">
-                        <NuxtLink :to=item.route v-if="(item.webOnly && settingsStore.isWeb) || !item.webOnly"
-                            class="text-2xl md:text-lg text-gray-800 rounded-lg md:hover:bg-emerald-100 hover:text-emerald-700 py-3 md:px-5 flex gap-3 items-center"
-                            activeClass="font-bold text-emerald-700 md:bg-emerald-100"
-                            router-link-active="font-bold text-emerald-700 bg-emerald-100">
-                            <UIIcons :name="item.icon" v-if="item.icon && showIcons"></UIIcons>
-                            {{ item.label }}
-
-                        </NuxtLink>
-                    </li>
-                </ul>
-            </div>
-
-        </nav>
-    </aside>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </aside>
 </template>
 
 <script setup lang="ts">
-import { Menu } from '~~/constants';
+import { Menu } from "~~/constants";
 import { useSettingsStore } from "~~/stores/settings";
-const settingsStore = useSettingsStore()
+const settingsStore = useSettingsStore();
 const showIcons = true;
 
 const Notifications = {
-    Tasks: 3
-}
+  Tasks: 3,
+};
 
 const menuState = ref(false);
 const toggleMenu = () => {
-    menuState.value = !menuState.value;
-}
-
+  menuState.value = !menuState.value;
+};
 </script>
 
 <script lang="ts">
 export default defineComponent({
-    name: 'Aside'
-})
+  name: "Aside",
+});
 </script>
