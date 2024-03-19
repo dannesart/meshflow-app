6ba2cb710616bb8b628aa18411771bf841aa6be7
dannesart <dannesart@live.com>
2024-03-19 19:02:01 +0100

    Correct auth layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="w-screen h-screen dark:bg-indigo-950">
+  <div class="w-screen h-screen dark:bg-indigo-900">
     <UILoadBar v-if="loading"></UILoadBar>
-    <main class="flex flex-col w-full h-full">
+    <main class="flex w-full h-full">
       <div class="flex-1 h-full p-10 bg-emerald-950">
-        <UILogo :size="30" />
+        <div class="flex gap-4 items-center">
+          <UILogo :size="30" />
+          <span
+            class="hidden text-xl font-extrabold md:block lg:text-2xl font-Rounded text-white"
+            >meshflow</span
+          >
+        </div>
       </div>
       <div class="flex-1 h-full p-10 bg-white">
         <slot />
