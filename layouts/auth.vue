3d62288d441d15aa98eee2b16f2f14e1d3d40043
dannesart <dannesart@live.com>
2024-03-27 18:06:50 +0100

    Update auth.vue

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -2,7 +2,7 @@
   <div class="w-screen h-screen dark:bg-indigo-900">
     <UILoadBar v-if="loading"></UILoadBar>
     <main class="flex flex-col w-full h-full md:flex-row">
-      <div class="px-10 py-2 md:flex-1 md:h-full md:py-10 bg-emerald-950">
+      <div class="px-10 py-4 md:flex-1 md:h-full md:py-10 bg-emerald-950">
         <div class="flex items-center gap-4">
           <UILogo :size="30" />
           <span
@@ -12,7 +12,7 @@
         </div>
       </div>
       <div
-        class="flex items-center justify-center h-full p-10 bg-white md:flex-1"
+        class="flex items-center justify-center h-full px-10 py-4 md:py-10 bg-white md:flex-1"
       >
         <div class="flex flex-col gap-5 flex-1 max-w-[500px]">
           <UIHeadline size="h1">
