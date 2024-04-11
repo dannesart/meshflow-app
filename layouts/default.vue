e04da76d15a1e60feac394da8451bbeb7d443bf3
dannesart <dannesart@live.com>
2024-04-11 02:39:03 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="layout bg-secondary-50 dark:bg-primary-950">
+  <div class="layout dark:bg-primary-950">
     <UILoadBar v-if="loading"></UILoadBar>
 
     <ModulesAside></ModulesAside>
     <ModulesHeader></ModulesHeader>
     <main
-      class="relative flex flex-col gap-10 px-6 py-4 pt-8 overflow-y-scroll overscroll-contain lg:px-10 lg:pt-12 lg:mb-10 lg:mr-10 bg-white dark:bg-black/20 rounded-3xl"
+      class="relative flex flex-col gap-10 px-6 py-4 pt-8 overflow-y-scroll overscroll-contain lg:px-10 lg:pt-12 lg:mb-10 lg:mr-10 bg-secondary-50 dark:bg-black/20 rounded-3xl"
     >
       <slot />
     </main>
