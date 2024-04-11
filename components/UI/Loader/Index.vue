13d51bb40f1f31d01ba3201b9f7c4a1c893bec04
dannesart <dannesart@live.com>
2024-04-11 21:04:51 +0200

    Update Index.vue

--- components/UI/Loader/Index.vue
+++ components/UI/Loader/Index.vue
@@ -1,11 +1,11 @@
 <template>
     <svg viewBox="0 0 36 36" class="w-14 aspect-square animated">
-        <path class="stroke-primary-200 dark:stroke-primary-950" d="M18 2.0845
+        <path class="stroke-primary-200 dark:stroke-secondary-950" d="M18 2.0845
           a 15.9155 15.9155 0 0 1 0 31.831
           a 15.9155 15.9155 0 0 1 0 -31.831" fill="none" stroke-width="4" />
         <path d="M18 2.0845
       a 15.9155 15.9155 0 0 1 0 31.831
-      a 15.9155 15.9155 0 0 1 0 -31.831" fill="none" :class="'stroke-primary-500'" stroke-width="4" stroke-linecap="round"
+      a 15.9155 15.9155 0 0 1 0 -31.831" fill="none" :class="'stroke-secondary-500'" stroke-width="4" stroke-linecap="round"
             :stroke-dasharray="'60, 100'" />
     </svg>
 </template>
