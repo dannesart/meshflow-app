60c28f555cfd61a0f530e2ee0c68fc121b5d6ac4
dannesart <dannesart@live.com>
2024-05-27 17:27:49 +0200

    type import fix

--- components/Modules/Edit/Block.vue
+++ components/Modules/Edit/Block.vue
@@ -1,69 +1,79 @@
 <template>
-    <UIForm :class="'flex flex-col gap-6'" name="new-block-form">
+  <UIForm :class="'flex flex-col gap-6'" name="new-block-form">
+    <ModulesInput
+      type="text"
+      :max="50"
+      :value="value?.name"
+      @valueUpdate="($event) => valueChange($event, 'name')"
+    >
+      Name
+    </ModulesInput>
 
-        <ModulesInput type="text" :max="50" :value="value?.name" @valueUpdate="$event => valueChange($event, 'name')">
-            Name
-        </ModulesInput>
+    <ModulesInput
+      type="text-lg"
+      :max="100"
+      :value="editBlockForm?.description"
+      @valueUpdate="($event) => valueChange($event, 'description')"
+    >
+      <label>
+        Description <span class="text-sm text-gray-400">Optional</span>
+      </label>
+    </ModulesInput>
 
-        <ModulesInput type="text-lg" :max="100" :value="editBlockForm?.description"
-            @valueUpdate="$event => valueChange($event, 'description')">
-            <label>
-                Description <span class="text-sm text-gray-400">Optional</span>
-            </label>
-        </ModulesInput>
-
-
-        <div name="new-block-error" class="flex gap-4 bg-red-50 p-4 rounded-lg mt-6" v-for="error in errors">
-            <span class="font-bold capitalize">
-                {{ error.path.join("/") }}
-            </span>
-            <span>
-                {{ error.message }}
-            </span>
-        </div>
-    </UIForm>
+    <div
+      name="new-block-error"
+      class="flex gap-4 bg-red-50 p-4 rounded-lg mt-6"
+      v-for="error in errors"
+    >
+      <span class="font-bold capitalize">
+        {{ error.path.join("/") }}
+      </span>
+      <span>
+        {{ error.message }}
+      </span>
+    </div>
+  </UIForm>
 </template>
 
 <script setup lang="ts">
-import { Model, ModelSchema } from '~~/models/model';
+import type { Model, ModelSchema } from "~~/models/model";
 
-const { value } = defineProps(['value']);
-const events = defineEmits(['valueUpdate', 'onValid', 'onError'])
+const { value } = defineProps(["value"]);
+const events = defineEmits(["valueUpdate", "onValid", "onError"]);
 const errors = ref();
 const editBlockForm = ref({ ...value });
 
 const valueChange = (event: string, key: string) => {
-    (editBlockForm as any).value[key] = event;
-    events('valueUpdate', editBlockForm);
-    validate(editBlockForm);
-}
+  (editBlockForm as any).value[key] = event;
+  events("valueUpdate", editBlockForm);
+  validate(editBlockForm);
+};
 
 const validate = async (editBlockForm: any) => {
-    const { name, description } = editBlockForm.value;
-    const toBeValidated = { name, description };
-    const validated = await ModelSchema.partial({
-        created: true,
-        createdBy: true,
-        updated: true,
-        updatedBy: true,
-        projectId: true,
-        fields: true,
-        id: true,
-        serviceType: true
-    }).safeParse(toBeValidated);
-    if (!validated.success) {
-        errors.value = [...validated.error.issues];
-        events('onError', validated.error)
-    } else {
-        errors.value = []
-        events('onValid', validated.data)
-    }
-}
-
+  const { name, description } = editBlockForm.value;
+  const toBeValidated = { name, description };
+  const validated = await ModelSchema.partial({
+    created: true,
+    createdBy: true,
+    updated: true,
+    updatedBy: true,
+    projectId: true,
+    fields: true,
+    id: true,
+    serviceType: true,
+  }).safeParse(toBeValidated);
+  if (!validated.success) {
+    errors.value = [...validated.error.issues];
+    events("onError", validated.error);
+  } else {
+    errors.value = [];
+    events("onValid", validated.data);
+  }
+};
 </script>
 
 <script lang="ts">
 export default defineComponent({
-    name: 'EditBlock'
-})
+  name: "EditBlock",
+});
 </script>
--- components/Modules/VisualEditor/Index.vue
+++ components/Modules/VisualEditor/Index.vue
@@ -1,79 +1,70 @@
 <template>
-    <div ref="editor">
-        <Tools :layer="layerStore.layer" @onChange="setClass">
-
-            <div class="ml-auto flex gap-3">
-                <p class="flex items-center p-3 text-sm text-slate-300">
-                    Selected layer: {{ layerStore.layer?.name }}
-                </p>
-                <UIButton type="icon" size="round-small" @click="addNewLayer()">
-                    <UIIcons name="add"></UIIcons>
-                </UIButton>
-                <UIButton type="icon" size="round-small" @click="toggleLayerNames()">
-                    <UIIcons :name="layerStore.showingLayerNames ? 'show' : 'hide'"></UIIcons>
-                </UIButton>
-            </div>
-        </Tools>
-        <div class="bg-slate-100 p-10 flex-1 rounded-lg relative">
-
-            <Layer v-for="layer in layers" :layer="layer"></Layer>
-        </div>
-
+  <div ref="editor">
+    <Tools :layer="layerStore.layer" @onChange="setClass">
+      <div class="ml-auto flex gap-3">
+        <p class="flex items-center p-3 text-sm text-slate-300">
+          Selected layer: {{ layerStore.layer?.name }}
+        </p>
+        <UIButton type="icon" size="round-small" @click="addNewLayer()">
+          <UIIcons name="add"></UIIcons>
+        </UIButton>
+        <UIButton type="icon" size="round-small" @click="toggleLayerNames()">
+          <UIIcons
+            :name="layerStore.showingLayerNames ? 'show' : 'hide'"
+          ></UIIcons>
+        </UIButton>
+      </div>
+    </Tools>
+    <div class="bg-slate-100 p-10 flex-1 rounded-lg relative">
+      <Layer v-for="layer in layers" :layer="layer"></Layer>
     </div>
+  </div>
 </template>
 
-
 <script setup lang="ts">
-
 import { defaultUtility } from "~~/constants/services/blocks";
-import { TLayer } from "~~/models/layer";
+import type { TLayer } from "~~/models/layer";
 import Tools from "./Tools.vue";
 import Layer from "./Layer.vue";
 import { useLayerStore } from "~~/stores/layer";
 import { COLORS } from "~~/constants/colors";
 
-const editor = ref()
+const editor = ref();
 const layerStore = useLayerStore();
 
-const { component } = defineProps(['component']);
+const { component } = defineProps(["component"]);
 const layers = ref<TLayer[]>([
-    component?.layers ||
-    {
-        type: defaultUtility,
-        name: '1',
-        layers: [],
-        classes: {
-            "Background": `bg-${COLORS[0]}`,
-            "Text": `text-${COLORS[4]}`,
-            "Padding": "p-5",
-            "Round": "rounded-xl",
-            "Display": "block"
-        }
-    }
+  component?.layers || {
+    type: defaultUtility,
+    name: "1",
+    layers: [],
+    classes: {
+      Background: `bg-${COLORS[0]}`,
+      Text: `text-${COLORS[4]}`,
+      Padding: "p-5",
+      Round: "rounded-xl",
+      Display: "block",
+    },
+  },
 ]);
 
-const setClass = ({ name, value }: { name: string, value: string }) => {
-    const layer = { ...layerStore.layer } as TLayer;
-    layer.classes[name] = value;
-    layerStore.setLayer(layer);
-}
+const setClass = ({ name, value }: { name: string; value: string }) => {
+  const layer = { ...layerStore.layer } as TLayer;
+  layer.classes[name] = value;
+  layerStore.setLayer(layer);
+};
 
 const toggleLayerNames = () => {
-    layerStore.toggleShowLayerNames();
-    layerStore.toggleShowLayerBorders();
-}
+  layerStore.toggleShowLayerNames();
+  layerStore.toggleShowLayerBorders();
+};
 
 const addNewLayer = () => {
-    layerStore.addNewLayer();
-}
+  layerStore.addNewLayer();
+};
 
 const initLayer = () => {
-
-    layerStore.setLayer(layers.value[0]);
-
-
-}
+  layerStore.setLayer(layers.value[0]);
+};
 initLayer();
-
-
 </script>
